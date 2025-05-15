d762e6e20d901b52ab6451b0547250c9a593f87a
Fariha Tahseen Karim <136506270+Farii01@users.noreply.github.com>
2025-05-15 19:10:43 +0200

    Update graph.py

--- sagan/graph.py
+++ sagan/graph.py
@@ -111,3 +111,57 @@ nx.draw(G, with_labels=True, font_weight='bold')
 plt.show()
 
 
+# degree part
+degrees = [deg for _, deg in G.degree()]
+print(degrees)
+
+#  histogram 
+plt.figure(figsize=(10, 6))
+plt.hist(degrees, bins=range(1, max(degrees)+2), edgecolor='black', color='skyblue')
+plt.title("Distribution of Node Degrees in ASN Graph")
+plt.xlabel("Degree (Number of Connections)")
+plt.ylabel("Number of ASNs")
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
+
+
+
+all_nodes = list(G.nodes()) # convert nodes to a list
+num_nodes = len(all_nodes)
+num_samples = 1000  #Total  Number of random node pairs to pick to find shortest path
+
+# Score: how many times each node appears in the middle of shortest paths
+score = defaultdict(int) # empty dictionary to keep count how many times the nodes apprears
+
+for _ in range(num_samples):
+    src, dst = random.sample(all_nodes, 2)# picking nodes
+    try:
+        path = nx.shortest_path(G, source=src, target=dst)#finding shortest path between the 2
+        for node in path[1:-1]:  # skip first and last
+            score[node] += 1
+    except nx.NetworkXNoPath:
+        continue  # no path between this pair, skip that pair and continues
+
+used_nodes = len(score) #how many nodes were used
+all_scores = list(score.values()) #list of all score 
+
+print(f"\nTotal nodes in graph: {num_nodes}")
+print(f"Nodes used in paths (score ≥ 1): {used_nodes}")
+
+# top asn with high score help us know important asns or central asns
+top = sorted(score.items(), key=lambda x: x[1], reverse=True)[:10]
+print("\nTop 10 nodes by score:")
+for node, val in top:
+    print(f"Node {node} → {val}")
+
+# Plot histogram
+plt.figure(figsize=(10, 6))
+plt.hist(all_scores, bins=30, edgecolor='black', color='skyblue')
+plt.title("Node Path Score Histogram")
+plt.xlabel("Times appeared in shortest paths")
+plt.ylabel("Number of nodes")
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.tight_layout()
+plt.show()
+
